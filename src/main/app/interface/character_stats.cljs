440b80a475b1f3ab865af9712cadadcfb060f49f
Kovas Palunas <kovas.palunas@gmail.com>
2023-06-15 09:59:10 -0700

    land aspects and leveling notes

--- src/main/app/interface/character_stats.cljs
+++ src/main/app/interface/character_stats.cljs
@@ -1,5 +1,9 @@
 (ns app.interface.character-stats)
 
+; TODO on leveling, have each character gain levels / experience in each
+; affinity proportional to the number of tiles of that land type they traversed
+; and faught on since their last level
+
 ; TODO make every class give a bonus to this value
 (defn get-max-health
   [character]
